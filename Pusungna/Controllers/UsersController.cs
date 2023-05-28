c82547b19833853e67f36b427d15462f426d5ae5
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-28 13:13:34 -0700

    Move UsersController from OrdersController.cs to UsersController.cs

---
+++ Pusungna/Controllers/UsersController.cs
@@ -0,0 +1,150 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Pusungna.Data;
+using Pusungna.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pusungna.Controllers
+{
+  public class UsersController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+
+    public UsersController(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    // GET: Users
+    public IActionResult Index()
+    {
+      return View(_context.Users.ToList());
+    }
+
+    // GET: Users/Details/5
+    public IActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var user = _context.Users
+        .FirstOrDefault(m => m.UserId == id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      return View(user);
+    }
+
+    // GET: Users/Create
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: Users/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([Bind("UserId,Username,Email,Password")] User user)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Add(user);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+      }
+      return View(user);
+    }
+
+    // GET: Users/Edit/5
+    public IActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var user = _context.Users.Find(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+      return View(user);
+    }
+
+    // POST: Users/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, [Bind("UserId,Username,Email,Password")] User user)
+    {
+      if (id != user.UserId)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(user);
+          _context.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!UserExists(user.UserId))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(user);
+    }
+
+    // GET: Users/Delete/5
+    public IActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var user = _context.Users
+        .FirstOrDefault(m => m.UserId == id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      return View(user);
+    }
+
+    // POST: Users/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+      var user = _context.Users.Find(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+      _context.Users.Remove(user);
+      _context.SaveChanges();
+      return RedirectToAction(nameof(Index));
+    }
+
+
+    private bool UserExists(int id)
+    {
+      return _context.Users.Any(e => e.UserId == id);
+    }
+  }
+}
