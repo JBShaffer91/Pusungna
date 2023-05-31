363e3eec3218484cf3103a4bd46f88f9bb450cb3
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-30 19:07:25 -0700

    Add BlogPost Views and update BlogPost Model
    In this commit, we accomplished the following:
    
    1. Created View files for the BlogPost controller, including Index.cshtml, Details.cshtml, Create.cshtml, Edit.cshtml, and Delete.cshtml.
    
    2. These views enable users to interact with blog posts in various ways, such as viewing a list of all blog posts (Index), viewing the details of a single blog post (Details), creating a new blog post (Create), editing an existing blog post (Edit), and deleting a blog post (Delete).
    
    3. Updated the BlogPost model to include the properties 'Summary', 'PublishDate', and 'Author'. These properties were required by the views for displaying and manipulating blog post data.
    
    4. Generated a new database migration and updated the database schema to reflect the changes made to the BlogPost model.

--- Pusungna/Models/BlogPost.cs
+++ Pusungna/Models/BlogPost.cs
@@ -6,9 +6,10 @@ namespace Pusungna.Models
     {
         public int BlogPostId { get; set; }
         public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Summary { get; set; }
         public string? Content { get; set; }
-        public DateTime PostDate { get; set; }
-        public int UserId { get; set; }
-        public User? User { get; set; }
+        public DateTime PublishDate { get; set; }
     }
 }
+
